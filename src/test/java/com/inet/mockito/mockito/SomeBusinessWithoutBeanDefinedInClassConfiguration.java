0086c8a7a3d8e99ae2f7ff2f76d06e632caa9326
bedril.moussakat <bedril.moussakat@accenture.com>
2020-12-31 11:46:54 +0400

    Explain override bean in the tests

--- src/test/java/com/inet/mockito/mockito/SomeBusinessWithoutBeanDefinedInClassConfiguration.java
+++ src/test/java/com/inet/mockito/mockito/SomeBusinessWithoutBeanDefinedInClassConfiguration.java
@@ -4,7 +4,6 @@ import com.inet.mockito.mockito.config.ConfigurationComponentScanWithoutBeanDefi
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -12,7 +11,6 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {ConfigurationComponentScanWithoutBeanDefined.class})
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class SomeBusinessWithoutBeanDefinedInClassConfiguration {
 
     @Autowired
@@ -24,6 +22,11 @@ public class SomeBusinessWithoutBeanDefinedInClassConfiguration {
     @Test
     public void test1() {
         int theGreatestFromAllData = someBusiness.findTheGreatestFromAllData();
+        /*
+        * Pour que ce test marche il faut commenter l'annotation @Configuration dans la classe ConfigurationWithBeanWithoutProfile.
+        * Puisque cette  configuration definit des beans qui override les beans definit scanner [@ComponentScan(basePackageClasses = {DataServiceImpl.class, SomeBusinessImpl.class})]
+        * dans la classe de configuration ConfigurationComponentScanWithoutBeanDefined. Ces beans etant ceux utiliser pendant l'execution du programme.
+        */
         assertEquals(Integer.MIN_VALUE, theGreatestFromAllData);
     }
 }
